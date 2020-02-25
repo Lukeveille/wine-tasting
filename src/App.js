f5126b3cfb2dcde4fffd650db8a319c72b60fd14
Lukeveille <luke@lukeleveille.com>
2020-02-24 19:05:36 -0500

    set up basic headers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [wine, setWine] = useState('Red');
+
+  console.log(wine)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select value ={wine} onChange={e => setWine(e.target.value)}>
+        <option value="Red">Red</option>
+        <option value="White">White</option>
+      </select>
+      <h1>{wine} Wine</h1>
+      <h2>Color</h2>
+      <h2>Fruit</h2>
+      <h2>Non-Fruit</h2>
+      <h2>Structure</h2>
+      <h2>Conclusion</h2>
+      <h2>Sales Pitch</h2>
     </div>
   );
 }
